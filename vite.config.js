d8991ca884c4934e92aeaca6c4b2c525be0a4c1c
Yx <hinskhor@gmail.com>
2025-06-06 19:53:53 +1200

    Add base

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/kids-math-learning/',
   plugins: [react(), tailwindcss(), mkcert()],
 })
